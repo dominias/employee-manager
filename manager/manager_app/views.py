a6532ede6323a3c2f968c7277231e387a2d9639e
Dominias <williamridenhourgaming@gmail.com>
2023-10-19 16:11:18 -0400

    Header and CSS
    The header that leads back to the home page, as well as general CSS.

--- manager/manager_app/views.py
+++ manager/manager_app/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
+from manager_app.models import User, CustomUserManager
 
-
-# Create your views here.
-def testFunc(request):
-    return HttpResponse("Manager App")
+def index(request):
+    return render(request, "index.html")
